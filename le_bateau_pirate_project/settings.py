f040aa3bef4f8fb05f3bde4a0397a9ccd352b58a
Yannick Thockler <yannick-thockler@cifacom.com>
2021-07-06 14:32:30 +0200

    ajout tupple pour STATICFILES_DIRS

--- le_bateau_pirate_project/settings.py
+++ le_bateau_pirate_project/settings.py
@@ -147,7 +147,9 @@ INTERNAL_IPS = ['127.0.0.1']
 if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATICFILES_DIRS = os.path.join(PROJECT_ROOT, 'static')
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static')
+    )
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
