7b9eadab4e86fb94e9b8c9eecbf4f3fabe60a8b3
Yannick Thockler <yannick-thockler@cifacom.com>
2021-07-06 16:07:25 +0200

    delete something

--- le_bateau_pirate_project/settings.py
+++ le_bateau_pirate_project/settings.py
@@ -159,5 +159,5 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-    db_from_env = dj_database_url.config()
+    db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
