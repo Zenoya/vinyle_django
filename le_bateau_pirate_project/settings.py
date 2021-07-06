9712226a70e57042cb4c99d3b6e3be88591cfd2e
Yannick Thockler <yannick-thockler@cifacom.com>
2021-07-06 14:34:38 +0200

    a

--- le_bateau_pirate_project/settings.py
+++ le_bateau_pirate_project/settings.py
@@ -145,11 +145,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = ['127.0.0.1']
 
 if os.environ.get('ENV') == 'PRODUCTION':
+
+    # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+    # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static')
+        os.path.join(PROJECT_ROOT, 'static'),
     )
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
